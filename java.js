8f320c08f02561a1869d9d1e7c5d5c1ab58de32e
Thapelo Mmakola <mthapelothapza@gmail.com>
2021-08-22 21:13:27 +0200

    change

--- java.js
+++ java.js
@@ -14,11 +14,15 @@ const largeAdd = document.querySelector(".largeAdd");
 const largeSubtract = document.querySelector(".largeSubtract");
 const largetotal = document.querySelector(".largetotal");
 
+const amount = document.querySelector(".amount");
+
 const Buy3 = document.querySelector(".Buy3");
 
 const grandTotal = document.querySelector(".grandTotal");
 
 const Check = document.querySelector(".Check");
+
+const Message = document.querySelector(".Message");
 
 //const Amount = document.querySelector(".Amount")
 
@@ -27,93 +31,103 @@ var addMedium = 0.00;
 var addLarge = 0.00;
 var addTotal = 0.00;
 
-smallAdd.addEventListener('click', function(){
-        addSmall += 39.99;
-        addTotal += 39.99;
-     smalltotal.innerHTML = "R" + addSmall.toFixed(2);
-     grandTotal.innerHTML = "R" + addTotal.toFixed(2);
-})
-
-smallSubtract.addEventListener('click', function(){
-            addSmall -= 39.99;
-            addTotal -= 39.99;
-            if(addSmall < 0) {
-                addSmall += 39.99;
-                addTotal += 39.99;
-                return;
-            }
+smallAdd.addEventListener('click', function () {
+    addSmall += 39.99;
+    addTotal += 39.99;
     smalltotal.innerHTML = "R" + addSmall.toFixed(2);
     grandTotal.innerHTML = "R" + addTotal.toFixed(2);
 })
 
-Buy1.addEventListener('click', function(){
+smallSubtract.addEventListener('click', function () {
+    addSmall -= 39.99;
+    addTotal -= 39.99;
+    if (addSmall < 0) {
+        addSmall += 39.99;
+        addTotal += 39.99;
+        return;
+    }
+    smalltotal.innerHTML = "R" + addSmall.toFixed(2);
+    grandTotal.innerHTML = "R" + addTotal.toFixed(2);
+})
+
+Buy1.addEventListener('click', function () {
     addSmall += 39.99;
     addTotal += 39.99;
- smalltotal.innerHTML = "R" + addSmall.toFixed(2);
- grandTotal.innerHTML = "R" + addTotal.toFixed(2);
+    smalltotal.innerHTML = "R" + addSmall.toFixed(2);
+    grandTotal.innerHTML = "R" + addTotal.toFixed(2);
 })
- ///Medium Pizza
- mediumAdd.addEventListener('click', function(){
+///Medium Pizza
+mediumAdd.addEventListener('click', function () {
     addMedium += 69.99;
     addTotal += 69.99;
- mediumtotal.innerHTML = "R" + addMedium.toFixed(2);
- grandTotal.innerHTML = "R" + addTotal.toFixed(2);
+    mediumtotal.innerHTML = "R" + addMedium.toFixed(2);
+    grandTotal.innerHTML = "R" + addTotal.toFixed(2);
 })
 
-mediumSubtract.addEventListener('click', function(){
-        addMedium -= 69.99;
-        addTotal -= 69.99;
-        if(addMedium < 0) {
-            addMedium += 69.99;
-            addTotal += 69.99;
-            return;
-        }
- mediumtotal.innerHTML = "R" + addMedium.toFixed(2);
-grandTotal.innerHTML = "R" + addTotal.toFixed(2);
+mediumSubtract.addEventListener('click', function () {
+    addMedium -= 69.99;
+    addTotal -= 69.99;
+    if (addMedium < 0) {
+        addMedium += 69.99;
+        addTotal += 69.99;
+        return;
+    }
+    mediumtotal.innerHTML = "R" + addMedium.toFixed(2);
+    grandTotal.innerHTML = "R" + addTotal.toFixed(2);
 })
 
-Buy2.addEventListener('click', function(){
-addMedium += 69.99;
-addTotal += 69.99;
-mediumtotal.innerHTML = "R" + addMedium.toFixed(2);
-grandTotal.innerHTML = "R" + addTotal.toFixed(2);
+Buy2.addEventListener('click', function () {
+    addMedium += 69.99;
+    addTotal += 69.99;
+    mediumtotal.innerHTML = "R" + addMedium.toFixed(2);
+    grandTotal.innerHTML = "R" + addTotal.toFixed(2);
 })
 
 //Large Pizza
 
-largeAdd.addEventListener('click', function(){
+largeAdd.addEventListener('click', function () {
     addLarge += 119.99;
     addTotal += 119.99;
- largetotal.innerHTML = "R" + addLarge.toFixed(2);
- grandTotal.innerHTML = "R" + addTotal.toFixed(2);
+    largetotal.innerHTML = "R" + addLarge.toFixed(2);
+    grandTotal.innerHTML = "R" + addTotal.toFixed(2);
 })
 
-largeSubtract.addEventListener('click', function(){
-        addLarge -= 119.99;
-        addTotal -= 119.99;
-        if(addLarge < 0) {
-            addLarge += 119.99;
-            addTotal += 119.99;
-            return;
-        }
-largetotal.innerHTML = "R" + addLarge.toFixed(2);
-grandTotal.innerHTML = "R" + addTotal.toFixed(2);
+largeSubtract.addEventListener('click', function () {
+    addLarge -= 119.99;
+    addTotal -= 119.99;
+    if (addLarge < 0) {
+        addLarge += 119.99;
+        addTotal += 119.99;
+        return;
+    }
+    largetotal.innerHTML = "R" + addLarge.toFixed(2);
+    grandTotal.innerHTML = "R" + addTotal.toFixed(2);
 })
 
-Buy3.addEventListener('click', function(){
-addLarge += 119.99;
-addTotal += 119.99;
-largetotal.innerHTML = addLarge.toFixed(2);
-grandTotal.innerHTML = addTotal.toFixed(2);
+Buy3.addEventListener('click', function () {
+    addLarge += 119.99;
+    addTotal += 119.99;
+    largetotal.innerHTML = addLarge.toFixed(2);
+    grandTotal.innerHTML = addTotal.toFixed(2);
+
+
 })
 
 //checkout
 
-Check.addEventListener('click', function(){
+Check.addEventListener('click', function () {
+
+
     if (addTotal > 39 && addTotal < 499) {
-        alert("Enjoy your pizzas");
+  Message.innerHTML = "Enjoy your pizzas";
+        
+      
+
+
     }
-  else if (addTotal >= 500) {
-         alert("you exceeded your budget")
-    } 
+    else if (addTotal >= 500) {
+        alert("you exceeded your budget")
+    }
+    
 })
+
